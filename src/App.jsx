093987baeec26300f8375705d84efa7564bb0d33
Débora Bastos <105684598+deborabastos96@users.noreply.github.com>
2024-03-14 20:38:11 -0300

    feat: style applayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Heading from "./ui/Heading";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-const StyledApp = styled.div`
-  color: #888;
-`;
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Heading as="h1">Hello World</Heading>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
